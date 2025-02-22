f6cee4ac381f2daa2f65533be1f6963271497f52
mirzakarlidag <mirzakarlidag@gmail.com>
2025-02-22 19:48:38 +0300

    LeetCode Daily Challenge Date: 22.02.25

--- DailyChallenge/22.02.25 - 1028. Recover a Tree From Preorder Traversal/main.py
+++ DailyChallenge/22.02.25 - 1028. Recover a Tree From Preorder Traversal/main.py
@@ -36,14 +36,48 @@ The number of nodes in the original tree is in the range [1, 1000].
 
 
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution(object):
     def recoverFromPreorder(self, traversal):
         """
         :type traversal: str
         :rtype: Optional[TreeNode]
         """
+        values = traversal.split("-")
+        root = TreeNode()
+
+        def backtrack(values, node):
+            for val in values:
+                if val:
+                    node.val = val
+                else:
+                    if not node.left:
+                        node.left = TreeNode(val)
+                    else:
+                        node.right = TreeNode(val)
+
+        backtrack(values, root)
+        print(values)
+        print(root.val, root.left.val, root.right.val)
+        return root
+
+
+root_1 = TreeNode(
+    1, TreeNode(2, TreeNode(3), TreeNode(4)), TreeNode(5, TreeNode(6), TreeNode(7))
+)
+root_2 = TreeNode(
+    1, TreeNode(2, TreeNode(3, TreeNode(4))), TreeNode(5, TreeNode(6, TreeNode(7)))
+)
+root_3 = TreeNode(1, TreeNode(401, TreeNode(349, TreeNode(90)), TreeNode(88)))
+
+sol = Solution()
+
+print(sol.recoverFromPreorder("1-2--3--4-5--6--7") == root_1)
+print(sol.recoverFromPreorder("1-2--3---4-5--6---7") == root_2)
+print(sol.recoverFromPreorder("1-401--349---90--88") == root_3)
